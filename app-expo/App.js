d385695d8f798f6cc324cd755c37c74f4db282a2
Karen Noguti <jkjvida@hotmail.com>
2023-03-14 21:03:40 +0900

    Pastas

---
+++ app-expo/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './src/Pages/HomePage'
+
+const App = () => {
+
+return (
+
+  <HomePage/>
+
+);
+
+}
+
+export default App;
